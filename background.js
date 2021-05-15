77e7d36378b6ea3ed5486ddecf076db804920f52
Jan Scheiper <janscheiper95@gmail.com>
2021-05-15 17:52:56 +0200

    version 1.24.0: upgraded cadmium playercore, added support for a new netflix cdn URL

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         return {
-            redirectUrl: chrome.extension.getURL("cadmium-playercore-0.0026.366.010-patched.js")
+            redirectUrl: chrome.extension.getURL("cadmium-playercore-6.0030.399.911-patched.js")
         };
     }, {
         urls: [
             "*://assets.nflxext.com/*/ffe/player/html/*",
-            "*://www.assets.nflxext.com/*/ffe/player/html/*"
+            "*://www.assets.nflxext.com/*/ffe/player/html/*",
+            "*://occ-weba-h2.a.nflxso.net/sec/*/ffe/player/html/*",
         ]
     }, ["blocking"]
 );
