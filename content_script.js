b4651459139283dd8b22fa83130f4e4176bca0fb
Jan Scheiper <janscheiper95@gmail.com>
2021-10-11 19:21:25 +0200

    bumped version to 1.25.1, finally removed msl_client, other small improvements

--- content_script.js
+++ content_script.js
@@ -1,12 +1,11 @@
 // From EME Logger extension
 
-script_urls = [
+const EXTERNAL_SCRIPTS = [
     // 'https://cdn.rawgit.com/ricmoo/aes-js/master/index.js',
     // 'https://cdn.rawgit.com/Caligatio/jsSHA/master/dist/sha.js'
 ];
 
-urls = [
-    // 'msl_client.js',
+const INTERNAL_SCRIPTS = [
     'netflix_max_bitrate.js'
 ];
 
@@ -18,8 +17,8 @@ function chromeStorageGet(opts) {
 } 
 
 function attachScript(resp) {
-    let xhr = resp.target;
-    let mainScript = document.createElement('script');
+    const xhr = resp.target;
+    const mainScript = document.createElement('script');
     mainScript.type = 'application/javascript';
     if (xhr.status == 200) {
         mainScript.text = xhr.responseText;
@@ -31,25 +30,24 @@ chromeStorageGet({
     use6Channels: true,
     setMaxBitrate: true,
     disableVP9: false,
-    disableAVChigh: false,
 }).then(items => {
     // very messy workaround for accessing chrome storage outside of background / content scripts
-    let mainScript = document.createElement('script');
+    const mainScript = document.createElement('script');
     mainScript.type = 'application/javascript';
     mainScript.text = `var globalOptions = JSON.parse('${JSON.stringify(items)}');`; 
     document.documentElement.appendChild(mainScript);
 }).then(() => {
     // attach and include additional scripts after we have loaded the main configuration
-    for (let i = 0; i < script_urls.length; i++) {
-        let script = document.createElement('script');
-        script.src = script_urls[i];
+    for (let i = 0; i < EXTERNAL_SCRIPTS.length; i++) {
+        const script = document.createElement('script');
+        script.src = EXTERNAL_SCRIPTS[i];
         document.documentElement.appendChild(script);
     }
     
-    for (let i = 0; i < urls.length; i++) {
-        let mainScriptUrl = chrome.extension.getURL(urls[i]);
+    for (let i = 0; i < INTERNAL_SCRIPTS.length; i++) {
+        const mainScriptUrl = chrome.extension.getURL(INTERNAL_SCRIPTS[i]);
     
-        let xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open('GET', mainScriptUrl, true);
         xhr.onload = attachScript;
     
